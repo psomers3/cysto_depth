06c8a9c0e97973bea8859eeebd5c597639000b8b
psomers <psvd3@umsystem.edu>
2022-11-11 08:38:01 +0100

    moved datamodule boilerplate to a parent class

--- data/depth_datamodule.py
+++ data/depth_datamodule.py
@@ -1,103 +1,39 @@
-from pathlib import Path
-import numpy as np
-import pytorch_lightning as pl
-from torch.utils.data import DataLoader
 from torchvision import transforms as torch_transforms
-import re
-import json
-from typing import *
 from data.image_dataset import ImageDataset
 import data.data_transforms as d_transforms
+from general_data_module import FileLoadingDataModule
 
-_mac_regex = re.compile(r'^(?!.*\._)')
 
-
-class DepthDataModule(pl.LightningDataModule):
+class EndoDepthDataModule(FileLoadingDataModule):
     def __init__(self,
                  batch_size,
                  color_image_directory: str,
                  depth_image_directory: str,
                  split: dict = None,
-                 image_size: Tuple[int, int] = (256, 256),
+                 image_size: int = 256,
                  workers_per_loader: int = 6):
-        """
+        """ A Data Module for loading rendered endoscopic images with corresponding depth maps. The color images should
+        be stored in a different directory as the depth images. See the split parameter for thoughts on how best to
+        set up your data structure for use with this module. The images will be made square and a circular mask applied
+        to simulate actual endoscopic images.
+        TODO: set up to use a configuration for control of all transforms and other hyperparams.
 
         :param batch_size: batch sized to use for training
         :param color_image_directory: path to the color images. Will be searched recursively
         :param depth_image_directory: path to the depth images. Will be searched recursively
-        :param split: can be one of the following:
-            - dictionary of regex strings to filter the filenames with:
-                {'train': ".*train.*", 'validate': ".*val.*", 'test': ".*test.*"} <- i.e. if divided into subfolders
-            - dictionary of floats specifying the random data split:
-                {'train': 0.5, 'validate': 0.3, 'test': 0.2}
-            - A combination of the two previous options:
-                {'train': 0.75, 'validate': 0.25, 'test': ".*model_01.*"}
-                The files for any regex matches will be assigned first and the remainder will respect the float splits
-            - string pointing to a file from a previously saved split
-            defaults to the first example assuming subfolders with "train", "val", and "test"
-        :param image_size: final image size to return for training.
+        :param split: see parent class FileLoadingDataModule
+        :param image_size: final `square` image size to return for training.
         :param workers_per_loader: cpu threads to use for each data loader.
         """
-        super().__init__()
+
+        directories = {'color': color_image_directory, 'depth': depth_image_directory}
+        super().__init__(batch_size, directories, split, workers_per_loader)
         self.save_hyperparameters("batch_size")
-        self.workers_per_loader = workers_per_loader
-        self.batch_size = batch_size
         self.image_size = image_size
-        if split is None:
-            split = {'train': ".*train.*", 'validate': ".*val.*", 'test': ".*test.*"}
-
-        if isinstance(split, str):
-            with open(split, 'r') as f:
-                self.split_files = json.load(f)
-        else:
-            image_files = {
-                'color': [str(f) for f in Path(color_image_directory).rglob('*') if _mac_regex.search(str(f))],
-                'depth': [str(f) for f in Path(depth_image_directory).rglob('*') if _mac_regex.search(str(f))]}
-            assert len(image_files['color']) == len(image_files['depth'])
-            image_files['color'].sort()
-            image_files['depth'].sort()
-            self.split_files = {'test': {}, 'validate': {}, 'train': {}}
-            stages = list(self.split_files.keys())
-            for stage in stages:
-                if isinstance(split[stage], str):
-                    test_re = re.compile(split[stage])
-                    for key, file_list in image_files.items():
-                        self.split_files[stage][key] = [s for s in file_list if test_re.search(s)]
-                        for f in self.split_files[stage][key]:
-                            file_list.remove(f)
-
-            indices = np.random.permutation(np.linspace(0,
-                                                        len(image_files['color']) - 1,
-                                                        len(image_files['color']))).astype(int)
-            remaining_count = len(indices)
-            for stage in stages:
-                if isinstance(split[stage], float):
-                    stage_indices = indices[:int(split[stage] * remaining_count) + 1]
-                    for key, file_list in image_files.items():
-                        self.split_files[stage][key] = np.asarray(file_list)[stage_indices]
-                    indices = indices[len(stage_indices):]
-
-        self.data_train: ImageDataset = None
-        self.data_val: ImageDataset = None
-        self.data_test: ImageDataset = None
-
-    def save_split(self, file_name: str):
-        """
-        Write the generated split to a file. TODO: deal with relative/absolute paths for portability
-
-        :param file_name: the name of the file to write the split to.
-        """
-        if file_name[-4:] != 'json':
-            file_name = f'{file_name}.json'
-        with open(file_name, 'w') as f:
-            json.dump(self.split_files, f)
-
-    def prepare_data(self):
-        pass
 
     def setup(self, stage: str = None):
         # normalize = torch_transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])  # imagenet
-        squarify = d_transforms.Squarify(image_size=min(self.image_size))
+        squarify = d_transforms.Squarify(image_size=self.image_size)
         mask = d_transforms.SynchronizedTransform(transform=d_transforms.EndoMask(radius_factor=[0.9, 1.0]),
                                                   num_synchros=2, additional_args=[[None], [0]])
         affine_transform = d_transforms.SynchronizedTransform(transform=d_transforms.RandomAffine(degrees=(0, 360),
@@ -124,37 +60,16 @@ class DepthDataModule(pl.LightningDataModule):
                                                          self.split_files['test']['depth'])),
                                           transforms=[color_transforms, depth_transforms])
 
-    def train_dataloader(self) -> DataLoader:
-        return DataLoader(self.data_train,
-                          batch_size=self.batch_size,
-                          num_workers=self.workers_per_loader,
-                          shuffle=True,
-                          pin_memory=True)
-
-    def val_dataloader(self) -> DataLoader:
-        return DataLoader(self.data_val,
-                          batch_size=self.batch_size,
-                          num_workers=self.workers_per_loader,
-                          shuffle=False,
-                          pin_memory=True)
-
-    def test_dataloader(self) -> DataLoader:
-        return DataLoader(self.data_test,
-                          batch_size=self.batch_size,
-                          num_workers=self.workers_per_loader,
-                          shuffle=False,
-                          pin_memory=True)
-
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
     from utils.image_utils import matplotlib_show
     color_dir = r'/Users/peter/isys/output/color'
     depth_dir = r'/Users/peter/isys/output/depth'
-    dm = DepthDataModule(batch_size=3,
-                         color_image_directory=color_dir,
-                         depth_image_directory=depth_dir,
-                         split={'train': .6, 'validate': 0.4, 'test': ".*00015.*"})
+    dm = EndoDepthDataModule(batch_size=3,
+                             color_image_directory=color_dir,
+                             depth_image_directory=color_dir,
+                             split={'train': .6, 'validate': 0.4, 'test': ".*00015.*"})
     dm.setup('fit')
     loader = dm.train_dataloader()
     matplotlib_show(*next(iter(loader)))
---
+++ data/general_data_module.py
@@ -0,0 +1,119 @@
+from pathlib import Path
+import numpy as np
+import pytorch_lightning as pl
+from torch.utils.data import DataLoader
+import re
+import json
+from data.image_dataset import ImageDataset
+
+_mac_regex = re.compile(r'^(?!.*\._)')
+
+
+class FileLoadingDataModule(pl.LightningDataModule):
+    def __init__(self,
+                 batch_size,
+                 directories: dict,
+                 split: dict = None,
+                 workers_per_loader: int = 6):
+        """
+         A Data Module for loading paired files located in different directories. See the split parameter for
+         thoughts on how best to set up your data structure for use with this module.
+
+        :param batch_size: batch sized to use for training
+        :param directories: a dictionary with each key-pair as the data role and directory, respectively.\
+                            i.e. {'img': /my/path, 'label': /my/other/path}
+
+        :param split: can be one of the following:
+                     - dictionary of regex strings to filter the filenames with:
+                       {'train': ".*train.*", 'validate': ".*val.*", 'test': ".*test.*"} <- i.e. if divided into subfolders
+                     - dictionary of floats specifying the random data split:
+                       {'train': 0.5, 'validate': 0.3, 'test': 0.2}
+                     - A combination of the two previous options:
+                       {'train': 0.75, 'validate': 0.25, 'test': ".*model_01.*"}
+                       The files for any regex matches will be assigned first and the remainder will respect the float splits
+                     - string pointing to a file from a previously saved split. Needs to be a json file with a dictionary
+                       of the form json_dict[stage][role] = List[files], where stage is one of ['train', 'validate', 'test']
+                       and role matches the keys provided in `directories`.
+                     split defaults to the first example assuming subfolders with "train", "val", and "test"
+
+        :param workers_per_loader: cpu threads to use for each data loader.
+        """
+        super().__init__()
+        self.workers_per_loader = workers_per_loader
+        self.batch_size = batch_size
+        if split is None:
+            split = {'train': ".*train.*", 'validate': ".*val.*", 'test': ".*test.*"}
+
+        if isinstance(split, str):
+            with open(split, 'r') as f:
+                self.split_files = json.load(f)
+        else:
+            image_files = {key: [str(f) for f in Path(val).rglob('*') if _mac_regex.search(str(f))] for key, val in
+                           directories.items()}
+            if len(list(image_files.keys())) > 1:
+                list_lengths = set([len(image_files[key]) for key in image_files])
+                assert len(list_lengths) == 1, f'Different number of files found between data roles.'
+
+            [image_files[key].sort() for key in image_files]
+            self.split_files = {'test': {}, 'validate': {}, 'train': {}}
+            stages = list(self.split_files.keys())
+            for stage in stages:
+                if isinstance(split[stage], str):
+                    test_re = re.compile(split[stage])
+                    for key, file_list in image_files.items():
+                        self.split_files[stage][key] = [s for s in file_list if test_re.search(s)]
+                        for f in self.split_files[stage][key]:
+                            file_list.remove(f)
+            first_role = list(image_files.keys())[0]
+            indices = np.random.permutation(np.linspace(0, len(image_files[first_role]) - 1,
+                                                        len(image_files[first_role]))).astype(int)
+            remaining_count = len(indices)
+            for stage in stages:
+                if isinstance(split[stage], float):
+                    stage_indices = indices[:int(split[stage] * remaining_count) + 1]
+                    for key, file_list in image_files.items():
+                        self.split_files[stage][key] = np.asarray(file_list)[stage_indices]
+                    indices = indices[len(stage_indices):]
+
+        self.data_train: ImageDataset = None
+        self.data_val: ImageDataset = None
+        self.data_test: ImageDataset = None
+
+    def save_split(self, file_name: str):
+        """
+        Write the generated split to a file. TODO: deal with relative/absolute paths for portability
+
+        :param file_name: the name of the file to write the split to.
+        """
+        if file_name[-4:] != 'json':
+            file_name = f'{file_name}.json'
+        with open(file_name, 'w') as f:
+            json.dump(self.split_files, f)
+
+    def setup(self, stage: str = None):
+        """
+        setup must be overridden and set the datasets self.data_train, self.data_val, and self.data_test.
+        The files to provide to the datasets can be accessed from self.split_files.
+        """
+        raise NotImplementedError()
+
+    def train_dataloader(self) -> DataLoader:
+        return DataLoader(self.data_train,
+                          batch_size=self.batch_size,
+                          num_workers=self.workers_per_loader,
+                          shuffle=True,
+                          pin_memory=True)
+
+    def val_dataloader(self) -> DataLoader:
+        return DataLoader(self.data_val,
+                          batch_size=self.batch_size,
+                          num_workers=self.workers_per_loader,
+                          shuffle=False,
+                          pin_memory=True)
+
+    def test_dataloader(self) -> DataLoader:
+        return DataLoader(self.data_test,
+                          batch_size=self.batch_size,
+                          num_workers=self.workers_per_loader,
+                          shuffle=False,
+                          pin_memory=True)
