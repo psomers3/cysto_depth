4db6215366db5853a20d9c033bd5b99c61867ef2
psomers <psvd3@umsystem.edu>
2023-01-25 14:58:27 +0100

    got gan running again

--- cysto_depth.py
+++ cysto_depth.py
@@ -65,7 +65,7 @@ def cysto_depth(cfg: CystoDepthConfig) -> None:
                                     synth_split=split,
                                     image_size=config.image_size,
                                     workers_per_loader=config.num_workers)
-        model = GAN(adaptive_gating=config.adaptive_gating, image_gan=config.image_gan, **config.gan_config)
+        model = GAN(image_gan=config.image_gan, synth_config=config.synthetic_config, gan_config=config.gan_config)
         [trainer_dict.update({key: val}) for key, val in config.gan_config.items() if key in trainer_dict]
         trainer_dict.update({'callbacks': get_callbacks(config.gan_config.callbacks)})
 
--- models/gan_model.py
+++ models/gan_model.py
@@ -9,35 +9,35 @@ from models.discriminator import Discriminator
 from models.depth_model import DepthEstimationModel
 import socket
 from utils.image_utils import generate_heatmap_fig, set_bn_eval, generate_final_imgs
+from config.training_config import SyntheticTrainingConfig, GANTrainingConfig
+from argparse import Namespace
 
 
 class GAN(BaseModel):
     def __init__(
             self,
+            synth_config: SyntheticTrainingConfig,
+            gan_config: GANTrainingConfig,
             preadapted_model=None,
             image_gan: bool = False,
-            res_transfer=True,
-            adaptive_gating=False,
-            **kwargs
     ):
         super().__init__()
-        self.save_hyperparameters()
-        depth_model = DepthEstimationModel.load_from_checkpoint(kwargs.get('synthetic_base_model'), strict=False)
-        if res_transfer:
-            self.generator = AdaptiveEncoder(adaptive_gating)
-            if preadapted_model:
-                self.generator.load_state_dict(preadapted_model.encoder.state_dict())
-            elif depth_model is not None:
-                self.generator.load_state_dict(depth_model.encoder.state_dict(), strict=False)
-        else:
-            self.generator = VanillaEncoder()
-            if depth_model is not None:
-                self.generator.load_state_dict(depth_model.encoder.state_dict(), strict=False)
-        # self.generator.grad_upper_layers(False)
+        self.save_hyperparameters(Namespace(**gan_config))
+        synth_config.resume_from_checkpoint = ''
+        depth_model = DepthEstimationModel.load_from_checkpoint(gan_config.synthetic_base_model,
+                                                                strict=False,
+                                                                config=synth_config)
+        self.config = gan_config
+        self.generator = AdaptiveEncoder(gan_config.adaptive_gating) if gan_config.residual_transfer else VanillaEncoder()
+        if preadapted_model:
+            self.generator.load_state_dict(preadapted_model.encoder.state_dict())
+        elif depth_model is not None:
+            self.generator.load_state_dict(depth_model.encoder.state_dict(), strict=False)
+
         if depth_model is not None:
             self.depth_model = depth_model
         else:
-            self.depth_model = DepthEstimationModel()
+            self.depth_model = DepthEstimationModel(synth_config)
         self.depth_model.requires_grad = False
 
         d_in_shapes = [512, 256, 128, 64, 64]
@@ -52,7 +52,7 @@ class GAN(BaseModel):
     def forward(self, z, full_prediction=False):
         if full_prediction:
             encoder_outs, encoder_mare_outs = self.generator(z)
-            return self.depth_model.depth_decoder(encoder_outs)
+            return self.depth_model.decoder(encoder_outs)
         else:
             return self.generator(z)
 
@@ -66,18 +66,18 @@ class GAN(BaseModel):
 
         # x = synthetic image, z = real image
         x, z = batch
-        if self.global_step == 0:
-            self.logger.experiment.add_graph(self.generator, z)
+        # if self.global_step == 0:
+        #     self.logger.experiment.add_graph(self.generator, z)
         # train generator
-        if optimizer_idx == 0 and self.global_step >= self.hparams.warmup_steps:
+        if optimizer_idx == 0 and self.global_step >= self.config.warmup_steps:
             # output of encoder when evaluating a real image
             encoder_outs, encoder_mare_outs = self.generator(z)
-            decoder_outs_synth = self.depth_model.depth_decoder(encoder_outs)
+            decoder_outs_synth = self.depth_model.decoder(encoder_outs)
 
             img_out = decoder_outs_synth[3]
 
             # compare output levels to make sure they produce roughly the same output
-            if self.hparams.res_transfer:
+            if self.config.residual_transfer:
                 residual_loss = torch.mean(torch.stack(
                     encoder_mare_outs))  # torch.mean(torch.stack([torch.mean(torch.abs(tensor)) for tensor in encoder_res_outs]))
             else:
@@ -88,14 +88,14 @@ class GAN(BaseModel):
             feat_outs = encoder_outs[::-1][:len(self.d_feat_modules)]
             for idx, feature_out in enumerate(feat_outs):
                 valid_predicted = self.d_feat_modules[idx](feature_out).type_as(feature_out)
-                valid = torch.ones(valid_predicted.size()).type_as(feature_out) * self.hparams.d_max_conf
+                valid = torch.ones(valid_predicted.size()).type_as(feature_out) * self.config.d_max_conf
                 loss = self.adversarial_loss(valid_predicted, valid)
                 self.log("g_loss_feature_{}".format(idx), loss)
                 g_losses_feat.append(loss)
 
             valid_predicted_img = self.d_img(img_out).type_as(img_out)
             # use type as to transfer to GPU
-            valid_img = torch.ones(valid_predicted_img.size()).type_as(img_out) * self.hparams.d_max_conf
+            valid_img = torch.ones(valid_predicted_img.size()).type_as(img_out) * self.config.d_max_conf
             # binary cross entropy (patch gan)
 
             g_loss_img = self.adversarial_loss(valid_predicted_img, valid_img)
@@ -104,8 +104,8 @@ class GAN(BaseModel):
             g_loss_feat = torch.sum(torch.stack(g_losses_feat))
             g_loss_img = g_loss_img
             g_loss = g_loss_feat \
-                     + self.hparams.residual_loss_factor * residual_loss \
-                     + self.hparams.img_discriminator_factor * g_loss_img
+                     + self.config.residual_loss_factor * residual_loss \
+                     + self.config.img_discriminator_factor * g_loss_img
             self.log("g_loss", g_loss)
             self.log("g_skip_loss", g_loss_feat)
             self.log("g_res_loss", residual_loss)
@@ -114,7 +114,7 @@ class GAN(BaseModel):
         elif optimizer_idx > 0:
             if optimizer_idx == 1:
                 y = self.depth_model(x)[-1].detach()
-                y_hat = self.depth_model.depth_decoder(self.generator(z)[0])[-1].detach()
+                y_hat = self.depth_model.decoder(self.generator(z)[0])[-1].detach()
                 d = self.d_img
                 name = "img"
             else:
@@ -130,7 +130,7 @@ class GAN(BaseModel):
             fake_predicted = d(y_hat)
 
             # how well can it label as real?
-            valid = (torch.ones(valid_predicted.size()) * self.hparams.d_max_conf).type_as(y_hat)
+            valid = (torch.ones(valid_predicted.size()) * self.config.d_max_conf).type_as(y_hat)
 
             real_loss = self.adversarial_loss(valid_predicted, valid)
 
@@ -155,7 +155,7 @@ class GAN(BaseModel):
 
     def validation_step(self, batch, batch_idx):
         x, z = batch
-        y_hat = self.depth_model.depth_decoder(self.generator(z)[0])
+        y_hat = self.depth_model.decoder(self.generator(z)[0])
         img_unapdated = self.depth_model(z)[-1]
         img_adapted = y_hat[-1]
         plot_tensors = [z]  # img_adapted, img_unapdated, diff]
@@ -176,7 +176,7 @@ class GAN(BaseModel):
 
     def test_step(self, batch, batch_idx):
         x, z = batch
-        y_hat = self.depth_model.depth_decoder(self.generator(z)[0])
+        y_hat = self.depth_model.decoder(self.generator(z)[0])
         img_unapdated = self.depth_model(z)[-1]
         img_adapted = y_hat[-1]
         plot_tensors = [z]  # img_adapted, img_unapdated, diff]
@@ -207,10 +207,10 @@ class GAN(BaseModel):
             plt.close(fig)
 
     def configure_optimizers(self):
-        lr_d = self.hparams.discriminator_lr
-        lr_g = self.hparams.generator_lr
-        b1 = self.hparams.beta_1
-        b2 = self.hparams.beta_2
+        lr_d = self.config.discriminator_lr
+        lr_g = self.config.generator_lr
+        b1 = self.config.beta_1
+        b2 = self.config.beta_2
 
         opt_g = torch.optim.Adam(filter(lambda p: p.requires_grad, self.generator.parameters()), lr=lr_g,
                                  betas=(b1, b2))
@@ -219,7 +219,7 @@ class GAN(BaseModel):
                                           betas=(b1, b2)) for discriminator in self.d_feat_modules]
         opt_d_img = torch.optim.Adam(filter(lambda p: p.requires_grad, self.d_img.parameters()), lr=lr_d,
                                      betas=(b1, b2))
-        if self.hparams.res_transfer:
+        if self.config.residual_transfer:
             milestones = [10, 20, 30]
         else:
             milestones = [10, 20, 30]
@@ -230,10 +230,10 @@ class GAN(BaseModel):
         return [opt_g, opt_d_img, *opt_d_feature], [lr_scheduler_g, lr_scheduler_d_img, *lr_schedulers_d_feat]
 
     def _on_epoch_end(self):
-        # if self.lr_schedulers() is not None:
-        # self.log("g_lr",self.lr_schedulers()[0].get_last_lr()[0])
-        # self.log("d_lr",self.lr_schedulers()[1].get_last_lr()[0])
-        self.log(socket.gethostname(), True)
+        if self.lr_schedulers() is not None:
+            self.log("generator_lr", self.lr_schedulers()[0].get_last_lr()[0])
+            self.log("discriminator_lr", self.lr_schedulers()[1].get_last_lr()[0])
+        # self.log(socket.gethostname(), True)
 
     def on_validation_epoch_end(self) -> None:
         self._on_epoch_end()
