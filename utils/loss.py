533c5ae4db2650ab1a20a3258012e871653da8cf
psomers <psvd3@umsystem.edu>
2023-01-28 12:17:19 +0100

    switched berhu reduction to mean

--- utils/loss.py
+++ utils/loss.py
@@ -41,7 +41,7 @@ class BerHu(nn.Module):
         part2 = part2 / (2. * delta)
 
         loss = part1 + part2
-        loss = torch.sum(loss)
+        loss = torch.mean(loss)
         return loss
 
 
