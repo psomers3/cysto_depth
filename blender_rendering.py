fixed camera resolution scaling

blender_rendering.py
@@ -56,7 +56,7 @@ def blender_rendering():
 
     cam_matrix = np.asarray(json.load(open(config.camera_intrinsics, 'r'))['IntrinsicMatrix']).T
     camera, cam_data = get_blender_camera_from_3x3_P(cam_matrix, scene=scene, clip_limits=[0.001, 0.5],
-                                                     scale=config.blender.render.resolution_percentage / 100)
+                                                     scale=config.blender.render.resolution_percentage/100)
     butils.apply_transformations(camera)
     scene.camera = camera
 