9d5c2512470471eca06ca5e8135904fc2bb003e6
psomers <psvd3@umsystem.edu>
2023-02-03 13:01:51 +0100

    uncommented transforms

--- data/phong_datamodule.py
+++ data/phong_datamodule.py
@@ -120,18 +120,18 @@ class PhongDataModule(FileLoadingDataModule):
         normals_rotation = d_transforms.MatrixRotation(Rotation.from_euler('XYZ', [0, 180, 0], degrees=True).as_matrix())
         normals_transforms = [d_transforms.FlipBRGRGB(), normals_rotation, normals_depth_squarify, mask]
         if split_stage == "train":
-            # affine = d_transforms.SynchronizedTransform(d_transforms.PhongAffine(degrees=(0, 359),
-            #                                                                      translate=(0, 0),
-            #                                                                      image_size=self.image_size),
-            #                                             num_synchros=self.num_synchros,
-            #                                             additional_args=[[True],
-            #                                                              [False, True],
-            #                                                              [False, True]])
+            affine = d_transforms.SynchronizedTransform(d_transforms.PhongAffine(degrees=(0, 359),
+                                                                                 translate=(0, 0),
+                                                                                 image_size=self.image_size),
+                                                        num_synchros=self.num_synchros,
+                                                        additional_args=[[True],
+                                                                         [False, True],
+                                                                         [False, True]])
             color_jitter = torch_transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1)
             color_transforms.insert(0, color_jitter)
-            # color_transforms.append(affine)
-            # depth_transforms.append(affine)
-            # normals_transforms.append(affine)
+            color_transforms.append(affine)
+            depth_transforms.append(affine)
+            normals_transforms.append(affine)
         color_transforms.append(imagenet_norm)
         color_transforms = torch_transforms.Compose(color_transforms)
         depth_transforms = torch_transforms.Compose(depth_transforms)
