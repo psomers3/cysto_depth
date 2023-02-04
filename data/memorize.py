ea82c25616d38c2fcc9b1abd14f73661278c53e6
Peter Somers <peter.w.somers@icloud.com>
2023-02-04 17:25:42 +0100

    fixed memorize

--- data/memorize.py
+++ data/memorize.py
@@ -6,13 +6,14 @@ class MemorizeCheck(Dataset):
         self.batch = batch
         self.length = length
         self.i = 0
-        self.num_items_in_batch = len(self.batch[0])
+        self.num_items_in_batch = len(self.batch)
+        self.batch_size = len(self.batch[0])
 
     def __len__(self):
         return self.length
 
     def __getitem__(self, idx):
-        if self.i == len(self.batch):
+        if self.i == self.batch_size:
             self.i = 0
         self.i += 1
-        return [self.batch[0][self.i - 1] for i in range(self.num_items_in_batch)]
+        return [self.batch[i][self.i - 1] for i in range(self.num_items_in_batch)]
