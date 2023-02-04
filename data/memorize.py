ee5b6280a24a0bee4c33af967e0429cb521e2047
psomers <psvd3@umsystem.edu>
2023-02-04 15:41:23 +0100

    made items in batch flexible

--- data/memorize.py
+++ data/memorize.py
@@ -6,6 +6,7 @@ class MemorizeCheck(Dataset):
         self.batch = batch
         self.length = length
         self.i = 0
+        self.num_items_in_batch = len(self.batch[0])
 
     def __len__(self):
         return self.length
@@ -14,4 +15,4 @@ class MemorizeCheck(Dataset):
         if self.i == len(self.batch):
             self.i = 0
         self.i += 1
-        return self.batch[0][self.i - 1], self.batch[1][self.i - 1]
+        return [self.batch[0][self.i - 1] for i in range(self.num_items_in_batch)]
