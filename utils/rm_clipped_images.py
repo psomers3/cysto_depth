f7ffde399fe44291fc955d3eb58b90079b96e5cc
psomers <psvd3@umsystem.edu>
2023-03-04 14:52:13 +0100

    added depth filter util

---
+++ utils/rm_clipped_images.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+
+from pathlib import Path
+from argparse import ArgumentParser
+from utils.exr_utils import exr_2_numpy
+from tqdm import tqdm
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('directory', type=str)
+    args = parser.parse_args()
+
+    directory = Path(args.directory)
+    files = [d for d in directory.rglob('*') if d.is_file() and d.suffix.lower() == '.exr']
+    for file in tqdm(files):
+        depth_map = exr_2_numpy(str(file))
+        if depth_map.max() >= 1000:
+            print(f'deleting: {file}')
+            file.unlink()
+
