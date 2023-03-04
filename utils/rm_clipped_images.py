c48d2df1c3384648a80382b13dd0e205c2c93bf6
psomers <psvd3@umsystem.edu>
2023-03-04 14:54:19 +0100

    added parent folder to path

--- utils/rm_clipped_images.py
+++ utils/rm_clipped_images.py
@@ -1,5 +1,9 @@
 #! /usr/bin/env python
 
+import sys
+import os
+
+sys.path.append(os.path.join('..', os.path.dirname(__file__)))
 from pathlib import Path
 from argparse import ArgumentParser
 from utils.exr_utils import exr_2_numpy
