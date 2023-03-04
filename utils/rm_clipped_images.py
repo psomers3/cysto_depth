8cf2926b4a4b00ab0a570874b6bb4ee50794679c
psomers <psvd3@umsystem.edu>
2023-03-04 15:12:24 +0100

    fixed relative import

--- utils/rm_clipped_images.py
+++ utils/rm_clipped_images.py
@@ -6,20 +6,21 @@ import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from pathlib import Path
 from argparse import ArgumentParser
-from .exr_utils import exr_2_numpy
+from utils.exr_utils import exr_2_numpy
 from tqdm import tqdm
 
 
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('directory', type=str)
+    parser.add_argument('max_depth', type=float, default=1e3)
     args = parser.parse_args()
 
     directory = Path(args.directory)
     files = [d for d in directory.rglob('*') if d.is_file() and d.suffix.lower() == '.exr']
     for file in tqdm(files):
         depth_map = exr_2_numpy(str(file))
-        if depth_map.max() >= 1000:
+        if depth_map.max() >= args.max_depth:
             print(f'deleting: {file}')
             file.unlink()
 
