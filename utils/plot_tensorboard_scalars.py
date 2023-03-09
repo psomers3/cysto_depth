7127ccf41719e3dd3fd61c0f3e8bf81475b2f36a
psomers <psvd3@umsystem.edu>
2023-03-09 17:10:26 +0100

    added smoothing for plotting

--- utils/plot_tensorboard_scalars.py
+++ utils/plot_tensorboard_scalars.py
@@ -2,11 +2,12 @@
 from argparse import ArgumentParser
 import os
 from typing import *
-
+from pathlib import Path
 from tensorboard_parsing import get_scalars_from_events
-
+import numpy as np
 import matplotlib.pyplot as plt
-plt.style.use(f'paper_style.mplstyle')
+import pandas
+plt.style.use(Path(Path(__file__).parent, f'paper_style.mplstyle'))
 plt.rcParams.update()
 
 # Paper sizes
@@ -30,7 +31,23 @@ class PlotData(TypedDict):
     ax: plt.Axes
 
 
-def get_plots(log_directories, tags, width, log) -> dict:
+def smooth(data:np.ndarray, tensorboard_factor: float = 0.6) -> np.ndarray:
+    """ apply exponential weighted MA """
+    # alpha = 1 - tensorboard_factor
+    # alpha_rev = 1-alpha
+    # n = data.shape[0]
+    # pows = alpha_rev**(np.arange(n+1))
+    # scale_arr = 1/pows[:-1]
+    # offset = data[0]*pows[1:]
+    # pw0 = alpha*alpha_rev**(n-1)
+    # mult = data*pw0*scale_arr
+    # cumsums = mult.cumsum()
+    # out = offset + cumsums*scale_arr[::-1]
+    df = pandas.DataFrame(data=data)
+    return df.ewm(alpha=(1 - tensorboard_factor)).mean().to_numpy()
+
+
+def get_plots(log_directories, tags, width, log) -> Dict[str, PlotData]:
     return_plots = {}
     data = [get_scalars_from_events(directory, tags) for directory in log_directories]
     for tag in tags:
@@ -40,10 +57,10 @@ def get_plots(log_directories, tags, width, log) -> dict:
         return_plots[tag]['fig'] = fig
         return_plots[tag]['ax'] = ax
         # Legend outside
-        percent_offset = 0.02
-        desired_width = fig.get_size_inches()[0]
+        # percent_offset = 0.02
+        # desired_width = fig.get_size_inches()[0]
         fig.tight_layout(h_pad=0, w_pad=0, pad=0)  # tight margins
-        dpi = fig.dpi
+        # dpi = fig.dpi
 
         for i, run in enumerate(data):
             for scalar_tup in run:
@@ -69,6 +86,7 @@ def get_plots(log_directories, tags, width, log) -> dict:
         if log:
             plt.yscale('log')
         legend = ax.legend(loc='best')
+        return_plots['legend'] = legend
         plt.draw()
     return return_plots
 
@@ -80,6 +98,7 @@ if __name__ == '__main__':
     parser.add_argument('--width', type=float, default=11.0, help='plot width in cm')
     parser.add_argument('--output', default='.', type=str, help='path to folder for output')
     parser.add_argument('--log', action='store_true', help='use log y-axis')
+    parser.add_argument('--smoothing', default=0.0, type=float, help='if >0 -> apply tensorboard smoothing')
 
     args = parser.parse_args()
     tags: List[str] = args.tags
@@ -88,4 +107,16 @@ if __name__ == '__main__':
     linestyle_str = ['solid', 'dotted', 'dashed', 'dashdot']
 
     for tag, items in get_plots(directories, tags, args.width, args.log).items():
+        if 0 < args.smoothing < 1:
+            new_lines = []
+            for line in items['ax'].lines:
+                line: plt.Line2D
+                x = line.get_xdata()
+                y = line.get_ydata()
+                line.set_alpha(0.2)
+                smoothed = smooth(y, args.smoothing)
+                new_lines.append(plt.Line2D(x, smoothed, linewidth=line.get_linewidth(),
+                                            linestyle=line.get_linestyle(), color=line.get_color()))
+            [items['ax'].add_line(nline) for nline in new_lines]
+            plt.draw()
         items['fig'].savefig(os.path.join(args.output, f'{tag}.pdf'), bbox_inches='tight', pad_inches=0.0, dpi='figure')
