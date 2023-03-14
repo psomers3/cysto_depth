226f0f3c04e87a85bdb5c847a55cdbb75d951db5
psomers <psvd3@umsystem.edu>
2023-03-14 16:06:32 +0100

    got got by a point

--- models/depth_model.py
+++ models/depth_model.py
@@ -298,7 +298,7 @@ class DepthEstimationModel(BaseModel):
             differences = synth_depths - y_hat_depth
 
             # TODO: tie this to a flag of synchronized gradient plots
-            minmax = [[None]]*self.max_num_image_samples
+            minmax = [[None] for _ in range(self.max_num_image_samples)]
             # minimums = [min(x.min().cpu(), y.min().cpu()) for x, y in zip(y_hat_depth, synth_depths)]
             maximums = [max(x.max().cpu(), y.max().cpu()) for x, y in zip(y_hat_depth, synth_depths)]
             [minmax[i].extend([(0, m), (0, m)]) for i, m in enumerate(maximums)]
